7f67f07b70f5aeaa07f86bc5832b2be1bffb63b4
AKSHAT KAMBOJ <akukamboj@gmail.com>
2025-07-28 18:43:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     pod_name = os.getenv('HOSTNAME', 'Unknown')
-    return f"Hello from Docker and Kubernetes! Running on Pod: {pod_name}\n"
+    return f"Helloo from Docker and Kubernetes! Running on Pod: {pod_name}\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
